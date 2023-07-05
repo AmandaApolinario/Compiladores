dcde99a1258c76442185c931ab07b810ec9a2928
kampuzzle <paulabremenkamp@gmail.com>
2023-07-05 20:46:04 -0300

    testei coisas

---
+++ T1/testes/variaveis.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a float32 
+	var b int 
+	var c bool 
+	var d string
+
+	a = 4.5
+	b = 5
+	// c = false
+	d = "teste"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	// fmt.Println(c)
+	fmt.Println(d)
+}
